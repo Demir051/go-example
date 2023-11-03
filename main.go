457195a4e4174d55f18d6680a308741f4408ac18
ademir <ahmetdemiraltunozz@gmail.com>
2023-11-04 02:33:32 +0300

    strucklarda kaldık

--- main.go
+++ main.go
@@ -5,5 +5,30 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World!")
+
+	// variables && types && pointers
+	var name string = "Demir"
+	var namePointer *string = &name
+	fmt.Println(name, namePointer, *namePointer)
+
+	fmt.Println(tr, usd, euro, gbp)
+
+	// arrays
+	arr := [...]int{1, 2, 3, 4, 5}
+	slice := arr[1:3]
+	fmt.Println("len =>", len(arr), "cap =>", cap(arr), "sliece =>", slice)
+
+	// maps
+	m := map[string]int{"istanbul": 1}
+	m["ankara"] = 2
+	fmt.Println(m, "/////", m["istanbul"], m["ankara"])
+
 }
+
+// iota
+const (
+	tr = iota + 1
+	usd
+	euro
+	gbp
+)
